02fa724cfee27636485e11c07ccfb88febf7bd7a
zenit1925 <kostia-85-85@yandex.ru>
2024-10-09 12:27:18 +0300

    1.1. Job c параметрами [#260360]

--- src/main/java/quartz/AlertRabbit.java
+++ src/main/java/quartz/AlertRabbit.java
@@ -2,53 +2,94 @@ package quartz;
 
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
-import java.io.IOException;
 import java.io.InputStream;
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
+
 import static org.quartz.JobBuilder.*;
 import static org.quartz.TriggerBuilder.*;
 import static org.quartz.SimpleScheduleBuilder.*;
 
 public class AlertRabbit {
+
+    public static int getInterval() {
+        try (InputStream in = AlertRabbit.class.getClassLoader().getResourceAsStream("rabbit.properties")) {
+            Properties config = new Properties();
+            config.load(in);
+            return Integer.parseInt(config.getProperty("rabbit.interval"));
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    public static Connection initConnection() {
+        try (InputStream input = AlertRabbit.class.getClassLoader()
+                .getResourceAsStream("rabbit.properties")) {
+            Properties config = new Properties();
+            config.load(input);
+            Class.forName(config.getProperty("driver-class-name"));
+            return DriverManager.getConnection(
+                    config.getProperty("db.url"),
+                    config.getProperty("db.username"),
+                    config.getProperty("db.password")
+            );
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
     public static void main(String[] args) {
-        try {
+        int interval = getInterval();
+        try (Connection conn = initConnection()) {
+            List<Connection> connection = new ArrayList<>();
+            connection.add(conn);
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
 
-            int interval = Integer.parseInt(loadProperties().getProperty("rabbit.interval", "10"));
+            JobDataMap data = new JobDataMap();
+            data.put("connection", connection);
 
-            JobDetail job = newJob(Rabbit.class).build();
+            JobDetail job = newJob(Rabbit.class)
+                    .usingJobData(data)
+                    .build();
+
             SimpleScheduleBuilder times = simpleSchedule()
-                    .withIntervalInSeconds(Integer.parseInt(String.valueOf(interval)))
+                    .withIntervalInSeconds(interval)
                     .repeatForever();
+
             Trigger trigger = newTrigger()
                     .startNow()
                     .withSchedule(times)
                     .build();
+
             scheduler.scheduleJob(job, trigger);
-        } catch (SchedulerException se) {
+
+            Thread.sleep(10000);
+            scheduler.shutdown();
+        } catch (Exception se) {
             se.printStackTrace();
         }
     }
 
     public static class Rabbit implements Job {
+        private void saveTime(Connection connection, LocalDateTime datetime) {
+            try (PreparedStatement statement =
+                         connection.prepareStatement("INSERT INTO rabbit(created_date) VALUES (?)")) {
+                statement.setTimestamp(1, Timestamp.valueOf(datetime));
+                statement.executeUpdate();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
         @Override
         public void execute(JobExecutionContext context) {
             System.out.println("Rabbit runs here ...");
+            List<Connection> conn = (List<Connection>) context.getJobDetail().getJobDataMap().get("connection");
+            saveTime(conn.get(0), LocalDateTime.now().withNano(0));
         }
-    }
-
-    private static Properties loadProperties() {
-        Properties properties = new Properties();
-        try (InputStream input = AlertRabbit.class.getClassLoader().getResourceAsStream("rabbit.properties")) {
-            if (input == null) {
-                System.out.println("Извините, не удалось найти rabbit.properties");
-                return properties;
-            }
-            properties.load(input);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return properties;
     }
 }
