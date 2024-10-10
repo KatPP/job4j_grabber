580b1bf65f3796b0cac7c15bc528efb6305ce96e
zenit1925 <kostia-85-85@yandex.ru>
2024-10-10 11:59:10 +0300

    2.1.1. Парсинг https://career.habr.com/vacancies/java_developer?page=1  [#285210]

--- src/main/java/utils/HabrCareerParse.java
+++ src/main/java/utils/HabrCareerParse.java
@@ -13,27 +13,28 @@ public class HabrCareerParse {
     private static final String SOURCE_LINK = "https://career.habr.com";
     public static final String PREFIX = "/vacancies?page=";
     public static final String SUFFIX = "&q=Java%20developer&type=all";
+    private static final int PAGES_PARSE = 5;
 
     public static void main(String[] args) throws IOException {
-        int pageNumber = 1;
-        String fullLink = "%s%s%d%s".formatted(SOURCE_LINK, PREFIX, pageNumber, SUFFIX);
-        Connection connection = Jsoup.connect(fullLink);
-        Document document = connection.get();
-        Elements rows = document.select(".vacancy-card__inner");
-        HabrCareerDateTimeParser dateTimeParser = new HabrCareerDateTimeParser();
+        for (int pageNumber = 1; pageNumber <= PAGES_PARSE; pageNumber++) {
+            String fullLink = "%s%s%d%s".formatted(SOURCE_LINK, PREFIX, pageNumber, SUFFIX);
+            Connection connection = Jsoup.connect(fullLink);
+            Document document = connection.get();
+            Elements rows = document.select(".vacancy-card__inner");
+            HabrCareerDateTimeParser dateTimeParser = new HabrCareerDateTimeParser();
 
-        rows.forEach(row -> {
-            Element titleElement = row.select(".vacancy-card__title").first();
-            Element datetime = row.select(".vacancy-card__date").first();
-            Element linkElement = titleElement.child(0);
-            String date = datetime.child(0).attr("datetime");
-            String vacancyName = titleElement.text();
-            String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+            rows.forEach(row -> {
+                Element titleElement = row.select(".vacancy-card__title").first();
+                Element datetime = row.select(".vacancy-card__date").first();
+                Element linkElement = titleElement.child(0);
+                String date = datetime.child(0).attr("datetime");
+                String vacancyName = titleElement.text();
+                String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+                OffsetDateTime offsetDateTime = dateTimeParser.parse(date);
+                String formattedDate = dateTimeParser.format(offsetDateTime);
 
-            OffsetDateTime offsetDateTime = OffsetDateTime.from(dateTimeParser.parse(date));
-            String formattedDate = dateTimeParser.format(offsetDateTime);
-
-            System.out.printf("Вакансия: - %s. Дата публикации: - %s. Ссылка: - %s%n", vacancyName, formattedDate, link);
-        });
+                System.out.printf("Вакансия: - %s. Дата публикации: - %s. Ссылка: - %s%n", vacancyName, formattedDate, link);
+            });
+        }
     }
 }
--- src/test/java/HabrCareerDateTimeParserTest.java
+++ src/test/java/HabrCareerDateTimeParserTest.java
@@ -1,11 +1,19 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.junit.jupiter.api.Test;
 import utils.DateTimeParser;
 import utils.HabrCareerDateTimeParser;
 
+import java.io.IOException;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
-import static org.assertj.core.api.Assertions.assertThat;
 import java.time.format.DateTimeParseException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class HabrCareerDateTimeParserTest {
     private final DateTimeParser parser = new HabrCareerDateTimeParser();
@@ -58,5 +66,52 @@ class HabrCareerDateTimeParserTest {
         String expectedFormattedDate = "2024-09-18T11:03:12";
         String actualFormattedDate = ((HabrCareerDateTimeParser) parser).format(dateTime);
         assertThat(actualFormattedDate).isEqualTo(expectedFormattedDate);
+    }
+
+    @Test
+    void whenParseFivePagesThenCorrectNumberOfVacancies() throws IOException {
+        List<String> vacancies = new ArrayList<>();
+        String sourceLink = "https://career.habr.com";
+        String prefix = "/vacancies?page=";
+        String suffix = "&q=Java%20developer&type=all";
+        int pagesToParse = 5;
+
+        for (int pageNumber = 1; pageNumber <= pagesToParse; pageNumber++) {
+            String fullLink = String.format("%s%s%d%s", sourceLink, prefix, pageNumber, suffix);
+            Document document = Jsoup.connect(fullLink).get();
+            Elements rows = document.select(".vacancy-card__inner");
+
+            for (Element row : rows) {
+                Element titleElement = row.select(".vacancy-card__title").first();
+                if (titleElement != null) {
+                    String vacancyName = titleElement.text();
+                    vacancies.add(vacancyName);
+                }
+            }
+        }
+        assertThat(vacancies).isNotEmpty();
+        assertThat(vacancies.size()).isGreaterThan(0);
+    }
+
+    @Test
+    void whenParseFirstPageThenCorrectVacanciesCount() throws IOException {
+        List<String> vacancies = new ArrayList<>();
+        String sourceLink = "https://career.habr.com";
+        String prefix = "/vacancies?page=";
+        String suffix = "&q=Java%20developer&type=all";
+
+        String fullLink = String.format("%s%s%d%s", sourceLink, prefix, 1, suffix);
+        Document document = Jsoup.connect(fullLink).get();
+        Elements rows = document.select(".vacancy-card__inner");
+
+        for (Element row : rows) {
+            Element titleElement = row.select(".vacancy-card__title").first();
+            if (titleElement != null) {
+                String vacancyName = titleElement.text();
+                vacancies.add(vacancyName);
+            }
+        }
+        assertThat(vacancies).isNotEmpty();
+        assertThat(vacancies.size()).isLessThanOrEqualTo(25);
     }
 }
